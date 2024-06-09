e6f0203b47998bf5415007deb0c4f59142e8cdba
osag1e <osagie6862@gmail.com>
2024-06-09 20:09:02 +0100

    feat: switch github account process tool completed.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # sap
 Switch Account Process (SAP) is a tool that simply allows you to switch between your Github accounts without the hassle of SSH.
+
+![sap](https://github.com/osag1e/sap/blob/main/sap.png)
+
+## Prerequisites after switiching account
+You need to generate a Personal Access Token (PAT) from your GitHub account developer settings and store it securely for authentication. You will need it to gain access to any of your Git operations after switching from one of your GitHub accounts to another. This needs to be generated for each of your GitHub accounts.
+
+- An example of why you need it is shown in the image below. You will need to use a PAT as you cannot use your github account password to authenticate.
+
+![authenticate](https://github.com/osag1e/sap/blob/main/authenticate.png)
+
+- You can see that the image prompts you to enter username and password. The password which requires a PAT does not show in the terminal when you enter it. This has been configured internally by git for security reasons. 
+
--- switch_account.sh
+++ switch_account.sh
@@ -1,11 +1,27 @@
 #!/bin/zsh
 
+# This prompts the user for their GitHub username and email
+echo "Enter your GitHub username:"
+read required_username
+echo "Enter your GitHub email:"
+read required_email
+
+# This fetches current Git global user name and email
+current_username=$(git config --global user.name)
+current_email=$(git config --global user.email)
+
+# This checks if the current configuration matches the required configuration
+if [ "$current_username" = "$required_username" ] && [ "$current_email" = "$required_email" ]; then
+  echo "The GitHub account is already set to the desired user."
+  exit 0
+fi
+
 # This sets global user name and email for GitHub account
-git config --global user.name "yourgithubusername"
-git config --global user.email "yourgithubemail@gmail.com"
+git config --global user.name "$required_username"
+git config --global user.email "$required_email"
 
 # If there are previously stored credentials for GitHub in your keychain, this
-# removes exising credentials for GitHub.
+# removes existing credentials for GitHub.
 security delete-internet-password -l "github.com"
 
 # This unsets credential helper configurations
@@ -13,6 +29,4 @@ git config --global --unset credential.helper
 git config --system --unset credential.helper
 
 echo "GitHub account switched successfully."
-
-
 
